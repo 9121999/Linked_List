517a0f1b5c517b8ee27414d390a552d63e171167
Prerana <dahiwadeprerana@gmail.com>
2022-11-20 17:11:57 +0530

    [prerana]Add:CreateList

---
+++ LinkedListDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkedListDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+        }
+    }
+}
